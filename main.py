bfed6fec1a2cc87624148107c25c02073f304a1e
igor <igor@email.com>
2024-11-23 22:55:17 -0500

    fixed https redirect problem

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from typing import List
-
 import uvicorn
-from fastapi import FastAPI, Form, UploadFile, File
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import estimate
 
 app = FastAPI()
 
-app.include_router(estimate.router)
+# Configuração do Middleware de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allow_headers=["*"],
+)
 
+app.include_router(estimate.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, log_config=None)
